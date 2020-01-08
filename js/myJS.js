ca336c886916950e73a642620d528583869968ef
lyushang <36389133+lyushang@users.noreply.github.com>
2020-01-08 18:28:31 -0500

    Add files via upload

--- js/myJS.js
+++ js/myJS.js
@@ -29,3 +29,65 @@ function changePlayer(n){
 function hoverEffect(){
     
 }
+
+var week_db=[["https://www.douyu.com/4615502","https://www.douyu.com/5524515","https://www.douyu.com/2947432","https://www.douyu.com/1457640","https://www.douyu.com/101367"],["images/week_rank_1.webp","images/week_rank_2.webp","images/week_rank_3.webp","images/week_rank_4.webp","images/week_rank_5.webp"],["一条小团团OvO","陈死狗cnh","呆妹儿小霸王","仙某某","轻语丶619"],["rankUnChange","rankIsUp","rankIsUp","rankIsDown","rankIsUp"]]
+var month_db=[["https://www.douyu.com/4615502","https://www.douyu.com/1457640","https://www.douyu.com/2947432","https://www.douyu.com/5524515","https://www.douyu.com/101367"],["images/week_rank_1.webp","images/week_rank_4.webp","images/week_rank_3.webp","images/week_rank_2.webp","images/week_rank_5.webp"],["一条小团团OvO","仙某某","呆妹儿小霸王","陈死狗cnh","轻语丶619"],["rankUnChange","rankUnChange","rankUnChange","rankUnChange","rankUnChange"]]
+
+function changeRank(){
+    var weekBut = document.getElementById("weekRank");
+    var monthBut = document.getElementById("monthRank"); 
+    
+    monthBut.onclick=function(){
+        monthBut.className="a-more active";
+        weekBut.className="a-more";
+        var url=month_db[0];
+        var image=month_db[1];
+        var name=month_db[2];
+        var rank=month_db[3];
+        var rankInfo = document.getElementsByClassName("rankInfo");
+        var rankIcon = document.getElementsByClassName("rankIcon");
+        var rankName = document.getElementsByClassName("rankName");
+        var rankChange = document.getElementsByClassName("rankChange");
+        
+        for(var i=0;i<5;i++){
+            rankInfo[i].href=url[i];
+            rankInfo[i].title=name[i];
+            rankIcon[i].childNodes[1].src=image[i];
+            rankName[i].innerHTML=name[i];
+            rankChange[i].childNodes[1].className=rank[i]; 
+        }
+    }
+    weekBut.onclick= function(){
+        weekBut.className="a-more active";
+        monthBut.className="a-more";
+        var url=week_db[0];
+        var image=week_db[1];
+        var name=week_db[2];
+        var rank=week_db[3];
+        var rankInfo = document.getElementsByClassName("rankInfo");
+        var rankIcon = document.getElementsByClassName("rankIcon");
+        var rankName = document.getElementsByClassName("rankName");
+        var rankChange = document.getElementsByClassName("rankChange");
+        
+        for(var i=0;i<5;i++){
+            rankInfo[i].href=url[i];
+            rankInfo[i].title=name[i];
+            rankIcon[i].childNodes[1].src=image[i];
+            rankName[i].innerHTML=name[i];
+            rankChange[i].childNodes[1].className=rank[i];
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
